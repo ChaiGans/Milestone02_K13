8476e7f8a4f31b021d26ed70c572d669ef3fb25e
SandWithCheese <naufalahmad022@gmail.com>
2023-08-07 08:19:06 +0700

    Updated to deploy url

--- src/src/app/api/auth/[...nextauth]/route.tsx
+++ src/src/app/api/auth/[...nextauth]/route.tsx
@@ -16,16 +16,19 @@ const handler = NextAuth({
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
-        const res = await fetch("http://localhost:3000/api/login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: credentials?.username,
-            password: credentials?.password,
-          }),
-        })
+        const res = await fetch(
+          "https://milestone-2-sparta.vercel.app/api/login",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              username: credentials?.username,
+              password: credentials?.password,
+            }),
+          }
+        )
 
         const user = await res.json()
 
